d7eb3fe4959ed7177db3fa3ac4f3950155664423
Arne Stysiak <a.stysiak@jung.de>
2025-07-21 14:37:55 +0200

    getting this somewhat to work?

--- custom_env.py
+++ custom_env.py
@@ -1,11 +1,51 @@
 import sinergym
 import gymnasium as gym
+import logging
+from sinergym.utils.logger import TerminalLogger
+import numpy as np
+
+# Logger
+terminal_logger = TerminalLogger()
+logger = terminal_logger.getLogger(
+    name='MAIN',
+    level=logging.INFO
+)
+
 
 env = gym.make("Eplus-2room-mild-continuous-v1")
 
-# Execute 3 episodes
-for i in range(3):
+print(env.action_space)
+print(env.observation_space)
+
+
+# Execute episodes
+for i in range(1):
 
     # Reset the environment to start a new episode
     obs, info = env.reset()
-    print(obs)
+
+    rewards = []
+    truncated = terminated = False
+    current_month = 0
+
+    while not (terminated or truncated):
+
+        # Random action selection
+        a = env.action_space.sample()
+
+        print("Action: ", a)
+
+        # Perform action and receive env information
+        obs, reward, terminated, truncated, info = env.step(a)
+
+        rewards.append(reward)
+
+        # Display results every simulated month
+        if info['month'] != current_month:
+            current_month = info['month']
+            logger.info('Reward: {}'.format(sum(rewards)))
+            logger.info('Info: {}'.format(info))
+
+    logger.info('Episode {} - Mean reward: {} - Cumulative Reward: {}'.format(i,
+                np.mean(rewards), sum(rewards)))
+env.close()
